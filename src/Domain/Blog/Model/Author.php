52b78b417181118abe3423c1200e497438776a7f
Yann Eugoné <yann.eugone@gmail.com>
2016-09-14 19:01:32 +0200

    Added User entity

---
+++ src/Domain/Blog/Model/Author.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Acme\Domain\Blog\Model;
+
+interface Author
+{
+    /**
+     * @return string
+     */
+    public function __toString();
+}
---
+++ src/Infrastructure/Bundle/AppBundle/Entity/User.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Acme\Infrastructure\Bundle\AppBundle\Entity;
+
+use Acme\Domain\Blog\Model\Author;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface, Author
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(type="array")
+     */
+    private $roles;
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+    }
+}
